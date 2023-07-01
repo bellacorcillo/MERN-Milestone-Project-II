d7928d660d1d3f04009b8e8c8fb233d45b708b65
Allen Go <goallen2001@gmail.com>
2023-07-01 11:15:44 -0700

    tried adding nav link

--- nevada/src/App.js
+++ nevada/src/App.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <h1>Hello, World!</h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <link to='/library'>Library</link>
+        </li>
+      </ul>
+      </Router>
     </div>
   );
 }
