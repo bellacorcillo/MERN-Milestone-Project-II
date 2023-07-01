9457e788f8f111e05b3b15e9158bd6e212d222f0
bellacorcillo <isabellacorcillo@gmail.com>
2023-07-01 11:14:06 -0700

    navigation elements

--- nevada/src/App.js
+++ nevada/src/App.js
@@ -1,13 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './pages';
+import Food from './pages/food';
+import Adventure from './pages/adventure';
+import Historical from './pages/historical';
+import Shopping from './pages/shopping';
+import Nightlife from './pages/nightlife';
 
+ 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' exact element={<Home />} />
+                <Route path='/food' element={<Food />} />
+                <Route path='/adventure' element={<Adventure />} />
+                <Route path='/shopping' element={<Shopping />} />
+                <Route path='/historical' element={<Historical />} />
+                <Route path='/nightlife' element={<Nightlife />} />
+            </Routes>
+        </Router>
+    );
 }
-
+ 
 export default App;
-
