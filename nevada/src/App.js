bd9d2dc6f46c27515098c85749b6e6713fd02aad
bellacorcillo <isabellacorcillo@gmail.com>
2023-07-01 13:04:40 -0700

    updates

--- nevada/src/App.js
+++ nevada/src/App.js
@@ -9,20 +9,22 @@ import Historical from './pages/historical';
 import Shopping from './pages/shopping';
 import Nightlife from './pages/nightlife';
 
-
 function App() {
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route exact path='/' exact element={<Home />} />
-                <Route path='/food' element={<Food />} />
-                <Route path='/adventure' element={<Adventure />} />
-                <Route path='/shopping' element={<Shopping />} />
-                <Route path='/historical' element={<Historical />} />
-                <Route path='/nightlife' element={<Nightlife />} />
-            </Routes>
-        </Router>
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/food" element={<Food />} />
+                    <Route path="/adventure" element={<Adventure />} />
+                    <Route path="/shopping" element={<Shopping />} />
+                    <Route path="/historical" element={<Historical />} />
+                    <Route path="/nightlife" element={<Nightlife />} />
+                </Routes>
+            </Router>
+            <h1 className="App-header"></h1>
+        </div>
     );
 }
 
