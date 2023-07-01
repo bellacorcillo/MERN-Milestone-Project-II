c24f36496e720143f48b6427d1a45a49d3435846
bellacorcillo <isabellacorcillo@gmail.com>
2023-07-01 11:32:54 -0700

    homepage title

--- nevada/src/pages/index.js
+++ nevada/src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to GeeksforGeeks</h1>
+            <h1>Las Vegas for Locals</h1>
         </div>
     );
 };
